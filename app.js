6da755f612cb39402ea8f3181610121204362f67
Rohit Sandip Vidhate <138186608+rohitvidhate7@users.noreply.github.com>
2025-12-10 09:57:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         // Prevent default anchor click behavior
         event.preventDefault();
 
-        // Store hash
+        // Store hash 
         var hash = this.hash;
 
         // Using jQuery's animate() method to add smooth page scroll
