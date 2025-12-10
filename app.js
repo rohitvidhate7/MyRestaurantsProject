f2a825acff631a8f3425cd5689844581ea5cb4e0
Rohit Sandip Vidhate <138186608+rohitvidhate7@users.noreply.github.com>
2025-12-10 09:57:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 
         // Store hash 
         var hash = this.hash;
-
+ 
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $("html, body").animate(
