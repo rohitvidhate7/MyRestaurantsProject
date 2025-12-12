bd0c9f58fc73b4eb44336cafd9d117ed6568b1b2
Rohit Sandip Vidhate <138186608+rohitvidhate7@users.noreply.github.com>
2025-12-12 17:49:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,9 @@ $(document).ready(function () {
     $("a").on("click", function (event) {
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
-        // Prevent default anchor click behavior
+ 
         event.preventDefault(); 
 
-        // Store hash 
         var hash = this.hash; 
   
     
